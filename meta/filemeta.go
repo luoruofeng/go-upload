15182296a5451bd5cfe665a8453ce17c75757073
luoruofeng <717750878@qq.com>
2022-02-23 11:02:49 +0800

    db

--- meta/filemeta.go
+++ meta/filemeta.go
@@ -1,6 +1,8 @@
 package meta
 
 import (
+	"sort"
+
 	db "github.com/luoruofeng/go-upload/db"
 )
 
@@ -29,4 +31,54 @@ func GetFileMeta(fileSha1 string) FileMeta {
 //insert and update from db
 func UpdateFileMetaDB(fmeta FileMeta) bool {
 	return db.OnFileUploadFinished(fmeta.FileSha1, fmeta.FileName, fmeta.FileSize, fmeta.Location)
+}
+
+// GetFileMetaDB : 从mysql获取文件元信息
+func GetFileMetaDB(fileSha1 string) (*FileMeta, error) {
+	tfile, err := db.GetFileMeta(fileSha1)
+	if tfile == nil || err != nil {
+		return nil, err
+	}
+	fmeta := FileMeta{
+		FileSha1: tfile.FileHash,
+		FileName: tfile.FileName.String,
+		FileSize: tfile.FileSize.Int64,
+		Location: tfile.FileAddr.String,
+	}
+	return &fmeta, nil
+}
+
+// GetLastFileMetas : 获取批量的文件元信息列表
+func GetLastFileMetas(count int) []FileMeta {
+	fMetaArray := make([]FileMeta, len(fileMetas))
+	for _, v := range fileMetas {
+		fMetaArray = append(fMetaArray, v)
+	}
+
+	sort.Sort(ByUploadTime(fMetaArray))
+	return fMetaArray[0:count]
+}
+
+// GetLastFileMetasDB : 批量从mysql获取文件元信息
+func GetLastFileMetasDB(limit int) ([]FileMeta, error) {
+	tfiles, err := db.GetFileMetaList(limit)
+	if err != nil {
+		return make([]FileMeta, 0), err
+	}
+
+	tfilesm := make([]FileMeta, len(tfiles))
+	for i := 0; i < len(tfilesm); i++ {
+		tfilesm[i] = FileMeta{
+			FileSha1: tfiles[i].FileHash,
+			FileName: tfiles[i].FileName.String,
+			FileSize: tfiles[i].FileSize.Int64,
+			Location: tfiles[i].FileAddr.String,
+		}
+	}
+	return tfilesm, nil
+}
+
+// RemoveFileMeta : 删除元信息
+func RemoveFileMeta(fileSha1 string) {
+	delete(fileMetas, fileSha1)
 }
